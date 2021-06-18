a1e0af59a4dbaf1d44d66f3d5877a932dd8b4fbf
Đỗ Ái Quốc <79491693+DoAiQuoc@users.noreply.github.com>
2021-06-18 22:15:09 +0700

    add controller

--- src/Controllers/ProductController.java
+++ src/Controllers/ProductController.java
@@ -1,6 +1,7 @@
-package com.productstore;
+package Controllers;
 
 import java.io.IOException;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
@@ -13,15 +14,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Models.Product;
+import Models.dbConnects;
 
-import com.productstore.Product;
-import com.productstore.ProductDAO;
 
-@WebServlet(urlPatterns= {"/update/*","/insert","/update","/list","/"})
-public class ControllerServlet extends HttpServlet {
+@WebServlet(urlPatterns= {
+		"/update-product/*", 
+		"/new-product",
+		"/insert-product",
+		"/edit-product",
+		"/list-product",
+		"/find-product",
+		"/delete-product"})
+public class ProductController extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-	private ProductDAO productDAO;
+	private dbConnects bd;
 	@Override
 	public void init() throws ServletException {
 		// TODO Auto-generated method stub
@@ -29,7 +37,7 @@ public class ControllerServlet extends HttpServlet {
 		String  url = "jdbc:mysql://localhost:3306/quan_li_kho";
 		String userName="root";
 		String pass="";
-		productDAO=new ProductDAO(url,userName,pass);
+		bd=new dbConnects(url,userName,pass);
 	}
 	
 	/**
@@ -39,20 +47,23 @@ public class ControllerServlet extends HttpServlet {
 		String action = request.getServletPath();
 		try {
 			switch (action) {
-			case "/insert":	
+			case "/insert-product":	
 				insertProduct(request,response);
 				break;
-			case "/new":
+			case "/new-product":
 				newForm(request, response);
 				break;
-			case "/update":
+			case "/update-product":
 				uppdateProduct(request,response);
 				break;
-			case "/edit":
+			case "/edit-product":
 				editForm(request,response);
 				break;
-			case "/delete":
+			case "/delete-product":
 				deleteProduct(request,response);
+				break;
+			case "/find-product":
+				findName(request, response);
 				break;
 			default:
 				listProduct(request, response);
@@ -69,14 +80,14 @@ public class ControllerServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	private void newForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dispatcher = request.getRequestDispatcher("ProductForm.jsp");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("Views/ProductForm.jsp");
 		dispatcher.forward(request, response);
 	}
 	private void editForm(HttpServletRequest request,HttpServletResponse response ) throws ServletException,IOException, SQLException{
 		int id_product = Integer.parseInt(request.getParameter("id"));
-		Product product = productDAO.getProduct(id_product);
+		Product product = bd.getProduct(id_product);
 		request.setAttribute("product",product);
-		RequestDispatcher dispatcher=request.getRequestDispatcher("ProductForm.jsp");
+		RequestDispatcher dispatcher=request.getRequestDispatcher("Views/ProductForm.jsp");
 		dispatcher.forward(request, response);
 
 	}
@@ -91,53 +102,50 @@ public class ControllerServlet extends HttpServlet {
 			int priceout = Integer.parseInt(request.getParameter("price_out"));
 			
 			Product product=new Product(id, nameProduct, code, number, priceInt, priceout);
-			productDAO.updateProduct(product);
-			response.sendRedirect("list");
+			bd.updateProduct(product);
+			response.sendRedirect("list-product");
 	
 	}
 	private void insertProduct(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException{
-
-
+		
 			String nameProduct=request.getParameter("product_name");
 			String code=request.getParameter("code");
 			int number = Integer.parseInt(request.getParameter("number"));
 			int priceInt = Integer.parseInt(request.getParameter("price_int"));
 			int priceout = Integer.parseInt(request.getParameter("price_out"));
 			Product product=new Product(nameProduct, code, number, priceInt, priceout);
-			productDAO.insertProduct(product);
-			response.sendRedirect("list");
+			bd.insertProduct(product);
+			response.sendRedirect("list-product");
 		
 	}
 	private void deleteProduct(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException{
 		int id=Integer.parseInt(request.getParameter("id"));		
 		Product product=new Product(id);
-		productDAO.deleteProduct(product);
-		response.sendRedirect("list");
+		bd.deleteProduct(product);
+		response.sendRedirect("list-product");
 	}
 	private void listProduct(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException,ServletException {
-		List<Product> listproduct=productDAO.getAllProduct();
+		List<Product> listproduct=bd.getAllProduct();
 		request.setAttribute("listProduct",listproduct);
-		RequestDispatcher dispatcher= request.getRequestDispatcher("ListProduct.jsp");
+		RequestDispatcher dispatcher= request.getRequestDispatcher("Views/ListProduct.jsp");
 		dispatcher.forward(request, response);
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private void findName(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException,ServletException {
 		String nameProduct = request.getParameter("nameproduct");
 		try {
-			List<Product> list = productDAO.getProductByName(nameProduct);
+			List<Product> list = bd.getProductByName(nameProduct);
 			request.setAttribute("listProduct",list);
 			
-			RequestDispatcher dispatcher=request.getRequestDispatcher("ListProduct.jsp");
+			RequestDispatcher dispatcher=request.getRequestDispatcher("Views/ListProduct.jsp");
 			dispatcher.forward(request, response);
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-		
-		
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 		doGet(request, response);
 	}
 
